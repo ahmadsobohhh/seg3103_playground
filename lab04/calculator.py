46691b5c52453d255e024799c7d89e276bc44779
ahmadsobohhh <asobo058@uottawa.ca>
2024-06-13 19:59:48 -0400

    Implement division to pass the test

--- lab04/calculator.py
+++ lab04/calculator.py
@@ -1,6 +1,11 @@
 class Calculator:
     def add(self, a, b):
         return a + b
+    
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Dividing by zero does not work")
+        return a / b
         
     def subtract(self, a, b):
         return a - b
