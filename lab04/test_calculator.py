c9d2ff69149135c92615ad9a62d68c751d5a2b76
ahmadsobohhh <asobo058@uottawa.ca>
2024-06-13 19:57:30 -0400

    Add failing test for subtraction

--- lab04/test_calculator.py
+++ lab04/test_calculator.py
@@ -4,7 +4,11 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         calc = Calculator()
         self.assertEqual(calc.add(2, 3), 5)
-
+    
+    def test_subtract(self):
+        calc = Calculator()
+        self.assertEqual(calc.subtract(5, 3), 2)
+    
 
 if __name__ == '__main__':
     unittest.main()
